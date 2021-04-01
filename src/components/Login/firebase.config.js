9dddd59a81905696d357ba8b4f4d3e7b1edfcb6e
abdulaziz899 <abdulaziz0170934@gmail.com>
2021-04-01 11:02:03 -0400

    complete

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyATFBwGrpNbqWDX07Sjq95otaPk9sVJZmI",
-    authDomain: "fullstact-client-site.firebaseapp.com",
-    projectId: "fullstact-client-site",
-    storageBucket: "fullstact-client-site.appspot.com",
-    messagingSenderId: "451635107946",
-    appId: "1:451635107946:web:f84cf814c565f0fc435213"
-  };
-
+  apiKey: "AIzaSyATFBwGrpNbqWDX07Sjq95otaPk9sVJZmI",
+  authDomain: "fullstact-client-site.firebaseapp.com",
+  projectId: "fullstact-client-site",
+  storageBucket: "fullstact-client-site.appspot.com",
+  messagingSenderId: "451635107946",
+  appId: "1:451635107946:web:f84cf814c565f0fc435213"
+};
   export default firebaseConfig;
+  
